9cb49af641632d2bde070995a38bd372b45fef41
Asilbek1111 <johnwickffa2003@gmail.com>
2025-07-12 15:45:23 +0500

    seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { Helmet } from "react-helmet";
 
 const App = () => {
-  return (
+  return (<>
+   <Helmet>
+        <title>Asilbekh.uz - 3d Portfolio</title>
+        <meta name="description" content="Welcome to the 3d Porfolio of Asilbek Haydarov!" />
+        <meta name="keywords" content="asilbekh uz, asilbek, haydarov, asilbek.uz, asilbekuz" />
+        <meta property="og:title" content="Asilbekh.uz" />
+      </Helmet>
+  
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
@@ -21,6 +29,7 @@ const App = () => {
         </div>
       </div>
     </BrowserRouter>
+  </>
   );
 }
 
